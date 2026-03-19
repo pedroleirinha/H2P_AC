99ea941e1731aabaffa9752b031f8778995354b8
Pedro Oleirinha <oleirinha.pedro@gmail.com>
2026-03-19 12:11:49 +0000

    removed a few lines that were redundant and added more comments

--- TLab1/tlab1.s
+++ TLab1/tlab1.s
@@ -1,53 +1,53 @@
-    .equ STACK_SIZE, 128
+    .equ STACK_SIZE, 64
     .equ VAL_MIN, 0x0005
     .equ MASK, 0x8000
 
     .text 
 
-    b program
-    b . ; Reservado (ISR)
+    B program
+    B . ; Reservado (ISR)
 
 program:
-    ldr sp, stack_top_addr
-    b main
+    LDR sp, stack_top_addr
+    B main
 
 stack_top_addr:
     .word stack_top
 
 ;START MAIN
 main:
-    push LR
+    PUSH LR
 
-    mov r0, #100
-    ldr r1, array_vals1_addr
-    ldr r2, array_k1_addr
-    ldr r3, array_s1_addr
+    MOV r0, #100
+    LDR r1, array_vals1_addr
+    LDR r2, array_k1_addr
+    LDR r3, array_s1_addr
 
-    bl build_sequence
+    BL build_sequence
     
-    mov r4, r0
+    MOV r4, r0
 
     ;n2
-    mov r0, #10
-    ldr r1, array_vals2_addr
-    ldr r2, array_k2_addr
-    ldr r3, array_s2_addr
+    MOV r0, #10
+    LDR r1, array_vals2_addr
+    LDR r2, array_k2_addr
+    LDR r3, array_s2_addr
 
-    bl build_sequence
+    BL build_sequence
 
-    mov r5, r0
+    MOV r5, r0
 
     ;n3
-    mov r0, #50
-    ldr r1, array_vals3_addr
-    ldr r2, array_k3_addr
-    ldr r3, array_s3_addr
+    MOV r0, #50
+    LDR r1, array_vals3_addr
+    LDR r2, array_k3_addr
+    LDR r3, array_s3_addr
 
-    bl build_sequence
+    BL build_sequence
     
-    mov r6, r0
+    MOV r6, r0
 
-    pop PC
+    POP PC
 
 ;END MAIN
 
@@ -68,53 +68,53 @@ main:
 ;   uint16_t ---------> r0
 ;
 build_sequence:
-    push LR ; prologo
-    push r4 ; prologo
-    push r5 ; prologo
-    push r6 ; prologo
-    push r7 ; prologo
+    PUSH LR ; prologo
+    PUSH r4 ; prologo
+    PUSH r5 ; prologo
+    PUSH r6 ; prologo
+    PUSH r7 ; prologo
 
-    mov r4, #0         ; uint16_t i = 0;
+    MOV r4, #0         ; uint16_t i = 0;
 
-    str r0, [r1]       ; v[0] = v_init;
+    STR r0, [r1]       ; v[0] = v_init;
 
-    mov r5, r1 ; V
-    mov r6, r2 ; K
-    mov r7, r3 ; S
+    MOV r5, r1 ; V
+    MOV r6, r2 ; K
+    MOV r7, r3 ; S
 
     ; ciclo while
 build_sequence_while:
 
-    mov r3, #0 ; i = 0
-    ldrb r2, [r7, r4] ;s[i]
+    MOV  r3, #0         ; i = 0
+    LDRB r2, [r7, r4]   ; s[i]
 
-    cmp r3, r2
-    beq build_sequence_while_end ; s[i] != 0
+    CMP  r3, r2
+    BEQ  build_sequence_while_end ; s[i] != 0
     
-    LSL r3, r4, #1
-    
-    ldr r0, [r5, r3]    ;v[i]
-    ldrb r1, [r6, r4]   ;k[i]
-    ldrb r2, [r7, r4]   ;s[i]
+    ;LSL r3, r4, #1             ;Não é preciso porque o valor de v[i] já está automaticamente no r0
+    ;LDR r0, [r5, r3]   ; v[i]  ;Não é preciso porque o valor de v[i] já está automaticamente no r0
 
-    BL scale_value
-    
-    ADD	r4, r4, #1 ; i++
-    LSL r3, r4, #1
 
-    STR r0, [r5, r3]
+    LDRB r1, [r6, r4]   ; k[i]
+    LDRB r2, [r7, r4]   ; s[i]
+    BL   scale_value
 
-    B	build_sequence_while
+    ADD	 r4, r4, #1     ; i++
+    LSL  r3, r4, #1     ; Faz ajuste para saltar 1 word completa em vez de 1 byte
+
+    STR  r0, [r5, r3]
+
+    B	 build_sequence_while
 
 build_sequence_while_end:
     
-    ADD	r0, r4, #1 ; i++
+    ADD	r0, r4, #1      ; i++
 
-    pop r7
-    pop r6
-    pop r5
-    pop r4 
-    pop PC
+    POP r7
+    POP r6
+    POP r5
+    POP r4 
+    POP PC
 ;END BUILD_SEQUENCE
 
 array_s1_addr:   .word array_s1
@@ -148,153 +148,170 @@ array_vals3_addr: .word array_vals3
 ;   uint16_t ---------> r0
 ;
 scale_value: 
-    ;r0 = v
-    ;r1 = k
-    ;r2 = s
+    PUSH LR         ; prologo
+    PUSH r4         ; prologo
+    PUSH r5         ; prologo
+    PUSH r6         ; prologo
+    PUSH r7         ; prologo
 
-    push LR ;prologo
-    push r4 ;prologo
-    push r5 ;prologo
-    push r6 ;prologo
-    push r7 ;prologo
+    MOV r4, r0      ; V
+    MOV r5, r1      ; K
+    MOV r6, r2      ; S
 
-    mov r4, r0 ; V
-    mov r5, r1 ; K
-    mov r6, r2 ; S
-
-    ;r0 e r1 = prod -- Não precisa de estar na stack porque não haverá mais chamadas a funções onde seja preciso perservar o valor
+                    ; r0 e r1 = prod -- 
+                    ; Não precisa de estar na stack porque não haverá mais chamadas à funções 
+                    ; onde seja preciso perservar o valor
     
-    MOV r2, #0xFF                ; k_ext = k & 0xFF; Mascara para tratar os bits
-    AND r1, r2, r1               ; menos significativos.    
+    MOV r2, #0xFF   ; k_ext = k & 0xFF; Mascara para tratar os bits
+    AND r1, r2, r1  ; menos significativos.    
 
-    ;; r0 = v[i]
-    ;; r1 = k_ext
+                    ; r0 = v[i]
+                    ; r1 = k_ext
 
-    bl multiplication
-    ; r0, r1 -> return
+    BL umull
+                    ; r0, r1 -> return
     
-    mov r2, #0  ; 
-    cmp r6, r2  ; if (s != 0)
-    beq scale_while_end02    ; 
+    MOV r2, #0      ;
+    CMP r6, r2      ; if (s != 0)
+    BEQ scale_while_end02    
 
-    ;operacoes com s
+    ; operacoes com s
 
-    MOV r2, #0xFF                ; k_ext = k & 0xFF; Mascara para tratar os bits
-    AND r6, r6, r2               ; menos significativos.    
+    MOV r2, #0xFF   ; k_ext = k & 0xFF; Mascara para tratar os bits
+    AND r6, r6, r2  ; menos significativos.    
 
 
-    mov r2, r6 ; S = 8
-    mov r3, #1
+    MOV r2, r6      ; S = 8
+    MOV r3, #1
 scale_before_while:
-    mov r4, #1 ; 1
+    MOV r4, #1
 scale_while:
-    cmp	r4, r2 ; S >= 1
-    bhs	scale_while_end 
+    CMP	r4, r2          ; S >= 1
+    BHS	scale_while_end 
     
-    lsl r3, r3, #1
-    sub	r2, r2, #1 ; m => contador; m--
-    b	scale_while
+    LSL r3, r3, #1
+    SUB	r2, r2, #1      ; m => contador; m--
+    B	scale_while
 
 scale_while_end:
 
-    add r0, r0, r3  ; 
-    mov r3, #0      ; prod += 0x00000001 << (s - 1);
-    adc r0, r0, r3  ; 
+    ADD r0, r0, r3  ; 
+    MOV r3, #0      ; prod += 0x00000001 << (s - 1);
+    ADC r0, r0, r3  ; 
 
-    mov r2, r6 ; S = 8
+    MOV r2, r6      ; S = 8
 scale_before_while02:
-    mov r4, #0 ; 1
+    MOV r4, #0 ; 1
 scale_while02:
-    cmp	r4, r2 ; S >= 1
-    bhs	scale_while_end02 
+    CMP	r4, r2              ; S >= 1
+    BHS	scale_while_end02 
     
-    lsr r1, r1, #1 ;MAIS SIGNIFICATIVO
-    RRX r0, r0 ;MENOS SIGNIFICATIVO
-    sub	r2, r2, #1 ; m => contador; m--
-    b	scale_while02
+    LSR r1, r1, #1          ; MAIS SIGNIFICATIVO
+    RRX r0, r0              ; MENOS SIGNIFICATIVO
+    SUB	r2, r2, #1          ; m => contador; m--
+    B	scale_while02
 
 scale_while_end02:
     ; prod = r0 e r1
 
     ; r0 -> prod_s ; r1 -> VAL_MIN; r2 -> r0
-    mov r1, #VAL_MIN
-    mov r2, r0
+    MOV r1, #VAL_MIN
+    MOV r2, r0
 
-    bl clamp_value_func    
+    BL clamp_value_func    
     ;retorno já esta no r0
 
-    pop r7
-    pop r6
-    pop r5
-    pop r4 
-    pop PC
+    POP r7
+    POP r6
+    POP r5
+    POP r4 
+    POP PC
 ;END SCALE_VALUE
 
 
-;START MULTIPLICATION
-multiplication:
-    ;r0 = v
-    ;r1 = k
-    ;r2,r3 = acc
-    
+;
+; >> Função UMULL <<
+; Tipo: - FOLHA -
+; Parametros de entrada:
+;   uint16_t v --> r0
+;   uint16_t k --> r1
+;
+; variaveis locais:
+;   uint16_t p -------> r2  ; Bits menos significativos
+;   uint16_t p -------> r3  ; Bits mais significativos
+;
+; Parametros de saida:
+;   uint32_t p -------> r0, r1
+;
+umull:
     PUSH R4
 
+                        ; compare_menor dos registos
+	CMP r0, r1
+	BHS before_while    ; aqui o r1 é menor que o r0
 
-    ;compare_menor dos registos
-	cmp r0, r1
-	bhs before_while ;aqui o r1 é menor que o r0
-
-    ;faz troca
-    mov r3, r0 ;mete o M no r4
-    mov r0, r1 ;mete no M o m
-    mov r1, r3 ;mete no m o r4  (que é o M)
+                        ; faz troca
+    MOV r3, r0          ; mete o M no r4
+    MOV r0, r1          ; mete no M o m
+    MOV r1, r3          ; mete no m o r4  (que é o M)
 
 before_while:
-    mov r4, #0
+    MOV r4, #0
     
-    mov r2, #0 ;acc[0]
-    mov r3, #0 ;acc[1]
+    MOV r2, #0          ; accumulado/p [0]
+    MOV r3, #0          ; accumulado/p [1]
 
 while:
-    cmp	r4, r1
-    bhs	while_end ; 0 >= m
+    CMP	r4, r1
+    BHS	while_end       ; 0 >= m
 
-    add	r2, r2, r0
-    mov r4, #0
-    adc r3, r3, r4
+    ADD	r2, r2, r0
+    MOV r4, #0
+    ADC r3, r3, r4
     
-    sub	r1, r1, #1 ; m => contador; m--
-    b	while
+    SUB	r1, r1, #1      ; m => contador; m--
+    B	while
 
 while_end:
 
     POP R4 
 
-    mov r0, r2
-    mov r1, r3
-    mov pc, lr ;return
+    MOV r0, r2
+    MOV r1, r3
+    MOV pc, lr          ; return do p no r0 e r1
 
-;END MULTIPLICATION
+;END UMULL
 
 
-;START CLAMP
+
+;
+; >> Função CLAMP_VALUE <<
+; Tipo: - FOLHA -
+; Parametros de entrada:
+;   uint16_t val --> r0
+;   uint16_t min --> r1
+;   uint16_t max --> r2
+;
+; variaveis locais:
+;   --Sem variáveis locais --
+;
+; Parametros de saida:
+;   uint32_t val -------> r0
+;
 clamp_value_func:
-    ;r0 = val
-    ;r1 = min
-    ;r2 = max
 
-    cmp r1, r0 ;if( val < min )
-    blt else
-    mov r0, r1
-    b return
+    CMP r1, r0  ; if( val < min )
+    BLT else
+    MOV r0, r1
+    B return
 else:
-    cmp r0, r2 ;if( val > max )
-    blt return
-    mov r0, r2
+    CMP r0, r2  ; if( val > max )
+    BLT return
+    MOV r0, r2
 
 return:
-    mov PC, LR
-;END CLAMP
+    MOV PC, LR
+;END CLAMP_VALUE
 
 
     .data
